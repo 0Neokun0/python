4acd55201786389c18540defdacb3f8d41158651
Your Name0Neokun0 <you@example.com>
2022-04-27 14:02:52 +0900

    Added Comments

--- picture-color-convert/tempCodeRunnerFile.py
+++ picture-color-convert/tempCodeRunnerFile.py
@@ -8,10 +8,7 @@ image.show()
 # RGBごとに分離しています。
 r, g, b  = image.split()
 
-# RGB → BGR に置き換え
-# 赤と青を入れ替えて合体(merge)している
+#
 convert_image = Image.merge("RGB", (b, g, r))
-
-# 変換後の画像保存
 convert_image.save(current_dirname + 'files/flower01_convert.jpg')
 convert_image.show()
