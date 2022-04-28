129bd38ee7c5c04954756582d06e6a0b9f2e06ba
Your Name0Neokun0 <you@example.com>
2022-04-28 15:23:17 +0900

    Keyword

---
+++ Learning-Python/Keyword-Identifiers/Statements-Comments/keyword.py
@@ -0,0 +1,46 @@
+from ast import Await
+from lib2to3.pytree import Node
+
+
+# Python Keywords are case sensitive.
+
+# Should be written as they are.
+True
+False
+None
+
+# The list of all the keywords which should be written in lowercase.
+
+Await
+else
+import
+pass
+break
+except
+in
+raise
+class
+finally
+is
+return
+and
+continue
+for
+lambda
+try
+as
+def
+from
+nonlocal
+while
+assert
+del
+global
+not
+with
+async
+elif
+if
+or
+yield
+
