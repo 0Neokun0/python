f1c5cc78ba139fe984be3a476738da4ff1f4d0bf
Your Name0Neokun0 <you@example.com>
2022-04-28 15:31:08 +0900

    Python Statements

---
+++ Learning-Python/Statements-Comments/python_statement.py
@@ -0,0 +1,29 @@
+# Multi-line Statement
+a = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9 
+
+a = (1 + 2 + 3 +
+     4 + 5 + 6 +
+     7 + 8 + 9)
+
+colors = ['red',
+          'blue',
+          'green']
+
+a = 1; b = 2; c =3
+
+# Python Indentation
+
+for i in range(1, 11):
+    print(i)
+    if i == 5:
+        break
+
+if True:
+    print('Hello')
+    a = 5
+    if True: print('Hello'); a = 5
+
+# Python comment
+print('Hello')
