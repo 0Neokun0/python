4219d3b47041b2ebf05f22d23f83a39fb28439c6
Your Name0Neokun0 <you@example.com>
2022-04-28 16:17:04 +0900

    Variables Constants And Literals

---
+++ Learning-Python/Variables-Constants-Literals/main.py
@@ -0,0 +1,24 @@
+# Python Variables
+from re import A
+
+
+number = 10
+number = 1.1
+
+# Declaring and assigning value to a variable
+website = "https://upcolor.weblike.jp/portfolio-1/"
+print (website)
+website = "www.google.com"
+print (website)
+
+# Assinging multiple values to multiple variables
+a , b, c = 2, 3.2, "Hello"
+print (a)
+print (b)
+print (c)
+
+# Assinging same values to multiple variables
+x = y = z = "same"
+print (x)
+print (y)
+print (z)
